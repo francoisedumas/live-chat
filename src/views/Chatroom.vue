92328328a207a7eb0a313aa0aeb2ef971f68bec1
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-09-13 19:53:36 +0200

    Redirect user from chatroom to welcome page on logout

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,8 +6,22 @@
 
 <script>
 import NavBar from '../components/NavBar.vue'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
+import { watch } from '@vue/runtime-core'
+
 export default {
-  components: { NavBar }
+  components: { NavBar },
+  setup() {
+    const { user } = getUser() 
+    const router = useRouter()
+
+    watch(user, ()  => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
+  }
 }
 </script>
 
